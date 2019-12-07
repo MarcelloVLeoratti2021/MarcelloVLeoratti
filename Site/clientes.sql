39eb4099e471b5c61cb23a8af32d85812c988a0f
MarcelloVLeoratti2021 <44924602+MarcelloVLeoratti2021@users.noreply.github.com>
2019-12-06 22:27:24 -0300

    Add files via upload

---
+++ Site/clientes.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS `acme`.`clientes` ( 
+`id` INT(11) NOT NULL AUTO_INCREMENT , 
+`nome` VARCHAR(50) NOT NULL , 
+`telefone` VARCHAR(11) NULL , 
+`email` VARCHAR(50) NOT NULL , 
+`cidade` VARCHAR(50) NOT NULL , 
+PRIMARY KEY (`id`)
+) 
+ENGINE = InnoDB;
